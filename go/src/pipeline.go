bd7761b5bb8e6da7e35e4914c14bc944eb499150
johan martinsson <jm1974@hotmail.com>
2020-06-15 22:56:38 +0200

    extracted stages, that only return an error

--- go/src/pipeline.go
+++ go/src/pipeline.go
@@ -10,42 +10,57 @@ type Pipeline struct {
 
 func (p *Pipeline) run(project Project) {
 
-	var err error = nil
-
-	if project.hasTests() {
-		if "success" == project.runTests() {
-			p.log.info("Tests passed")
-
-		} else {
-			err = errors.New("Tests failed")
-
-		}
-	} else {
-		p.log.info("No tests")
-
-	}
-
+	// for stages
+	err := p.runTestStage(project)
 	if err == nil {
-		if "success" == project.deploy() {
-			p.log.info("Deployment successful")
-
-		} else {
-			err = errors.New("Deployment failed")
-		}
-	} else {
+		err = p.runDeployStage(project)
 	}
 
-	var endResult = ""
+	// reporting
 	if err != nil {
 		p.log.error(err.Error())
-		endResult = err.Error()
-	} else {
-		endResult = "Deployment completed successfully"
 	}
+
+	result := p.computeEndResult(err)
+
+	// email or null object
 	if p.config.sendEmailSummary() {
 		p.log.info("Sending email")
-		p.emailer.send(endResult)
+		p.emailer.send(result)
 	} else {
 		p.log.info("Email disabled")
 	}
+}
+
+func (p *Pipeline) computeEndResult(err error) string {
+	var endResult = "Deployment completed successfully"
+	if err != nil {
+		endResult = err.Error()
+	}
+	return endResult
+}
+
+func (p *Pipeline) runTestStage(project Project) error {
+	err := error(nil)
+	// almost possible to move this to project class, if it wasnt for the lack of logging in the failure case
+	if project.hasTests() {
+		if "success" == project.runTests() {
+			p.log.info("Tests passed")
+		} else {
+			err = errors.New("Tests failed")
+		}
+	} else {
+		p.log.info("No tests")
+	}
+	return err
+}
+
+func (p *Pipeline) runDeployStage(project Project) error {
+	err := error(nil)
+	if "success" == project.deploy() {
+		p.log.info("Deployment successful")
+	} else {
+		err = errors.New("Deployment failed")
+	}
+	return err
 }
